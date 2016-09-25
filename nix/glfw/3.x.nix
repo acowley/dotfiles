b15d4c413bc0951f1b7e3c625646ae6ba793d92c
Anthony Cowley <acowley@gmail.com>
2016-09-25 16:22:00 -0400

    Build GLFW 3.2 against an Xcode SDK
    The nixpkgs frameworks are too old.

---
+++ nix/glfw/3.x.nix
@@ -0,0 +1,34 @@
+{ stdenv, fetchFromGitHub, cmake, mesa, libXrandr, libXi, libXxf86vm, libXfixes, xlibsWrapper
+, libXinerama, libXcursor, darwin
+}:
+
+stdenv.mkDerivation rec {
+  version = "3.2";
+  name = "glfw-${version}";
+
+  src = fetchFromGitHub {
+    owner = "glfw";
+    repo = "GLFW";
+    rev = "${version}";
+    sha256 = "0knqf40jij2z1mia091xqyky5r11r4qyh7b8172blrmgm9q23sl9";
+  };
+
+  enableParallelBuilding = true;
+
+  buildInputs = if stdenv.isDarwin then [cmake darwin.osx_sdk] else [
+    cmake mesa libXrandr libXi libXxf86vm libXfixes xlibsWrapper
+    libXinerama libXcursor
+  ];
+
+  cmakeFlags = ["-DBUILD_SHARED_LIBS=ON"] ++
+    stdenv.lib.optionals stdenv.isDarwin [
+      "-DCMAKE_OSX_SYSROOT='/'" "-DCMAKE_OSX_DEPLOYMENT_TARGET=''"];
+
+  meta = with stdenv.lib; {
+    description = "Multi-platform library for creating OpenGL contexts and managing input, including keyboard, mouse, joystick and time";
+    homepage = "http://www.glfw.org/";
+    license = licenses.zlib;
+    maintainers = with maintainers; [ marcweber ];
+    platforms = platforms.linux;
+  };
+}
