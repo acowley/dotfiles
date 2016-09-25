2f37b97572524c2311e1fa8fceca2d54b096ec52
Anthony Cowley <acowley@gmail.com>
2016-09-25 16:22:33 -0400

    Build irony-server to work with irony-mode
    This is the server component for a C++ emacs mode.

---
+++ nix/irony-server/default.nix
@@ -0,0 +1,18 @@
+{ stdenv, clang, fetchFromGitHub, cmake }:
+let SHLIB = if stdenv.isDarwin then "dylib" else "so"; in
+stdenv.mkDerivation {
+  name = "irony-server-2016-08-24";
+  # src = fetchFromGitHub {
+  #   owner = "Sarcasm";
+  #   repo = "irony-mode";
+  #   rev = "11bdf09b998c54be59bac8df3368039de2c0aa19";
+  #   sha256 = "1q0liagda4ch9ya5ic9flwprxv4wq23ws3kg00d4wg0y3x4czwhm";
+  # };
+  src = ../../src/irony-mode;
+  buildInputs = [ cmake ];
+  preConfigure = "cd server";
+  cmakeFlags = [
+    "-DLIBCLANG_LIBRARY=${stdenv.cc.cc.lib}/lib/libclang.${SHLIB}"
+    "-DLIBCLANG_INCLUDE_DIR=${stdenv.cc.cc.lib}/include"
+  ];
+}
