ca30f6b84a5766412ad2df09562a4ab86366cc99
Anthony Cowley <acowley@gmail.com>
2016-10-05 10:50:27 -0400

    Adopt fixed opencv3 in nixpkgs.

--- config.nix
+++ config.nix
@@ -106,44 +106,51 @@
                   in [ p.setuptools p.pip p.bottle p.platformio ];
     }).env;
 
-    opencv3 =
-      let mypy27 = pkgs.python27.buildEnv.override {
-            extraLibs = [ pkgs.python27Packages.numpy ];
-          };
-          mypy3 = pkgs.python3.buildEnv.override {
-            extraLibs = [ pkgs.python3Packages.numpy ];
-          };
-      in (pkgs.opencv3.override {enableContrib = true;}).overrideDerivation
-      (oldAttrs: {
-        buildInputs = (with pkgs; [ unzip libjpeg libpng libtiff ])
-          ++ [ pkgs.eigen pkgs.bzip2 mypy27 mypy3 pkgs.ffmpeg-full ];
-        nativeBuildInputs = oldAttrs.nativeBuildInputs
-          ++ [ pkgs.doxygen ]
-          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
-               (with pkgs.darwin.apple_sdk.frameworks;
-                [ AVFoundation OpenCL QuartzCore QTKit AppKit Cocoa ]);
-        preConfigure = ''
-          NIX_CFLAGS_COMPILE=$(echo "$NIX_CFLAGS_COMPILE" | ${pkgs.gnused}/bin/sed "s,[[:space:]]*-F$NIX_STORE/[[:alnum:]]*-CF-osx-[[:digit:].]*/Library/Frameworks,,g")
-          export PYTHON2_EXECUTABLE=${mypy27}/bin/python
-          export PYTHON2_LIBRARY=${mypy27}/lib/libpython2.7.dylib
-          export PYTHON2_INCLUDE_DIR=${mypy27}/include/python2.7
-          export PYTHON2_PACKAGES_PATH=${mypy27}/lib/python2.7/site-packages
-          export PYTHON3_EXECUTABLE=${mypy3}/bin/python3
-          export PYTHON3_LIBRARY=${mypy3}/lib/libpython3.5m.dylib
-          export PYTHON3_INCLUDE_DIR=${mypy3}/include/python3.5
-          export PYTHON3_PACKAGES_PATH=${mypy3}/lib/python3.5/site-packages
-          export EIGEN_ROOT=${pkgs.eigen}
-          ${pkgs.gnused}/bin/sed -i 's,\(^[[:space:]]*PATH_SUFFIXES include/eigen3 include/eigen2 Eigen/include/eigen3 Eigen/include/eigen2\),\1 eigen3,' ./cmake/OpenCVFindLibsPerf.cmake
-          ${pkgs.gnused}/bin/sed -i 's|    INSTALL_NAME_DIR lib|    INSTALL_NAME_DIR ''${CMAKE_INSTALL_NAME_DIR}|' ./cmake/OpenCVModule.cmake
-        '';
-        cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DWITH_FFMPEG=ON -DWITH_QT=OFF -DWITH_QUICKTIME=OFF -DWITH_AVFOUNDATION=ON" ];
-        NIX_CFLAGS_COMPILE="-DDEPLOYMENT_TARGET_MACOSX";
-        patches = [
-          (pkgs.fetchpatch {
-             url = https://github.com/opencv/opencv/commit/a2bda999211e8be9fbc5d40038fdfc9399de31fc.patch;
-             sha256 = "1dmq6gqsy1rzf57acr6kn4xy03vdmmk9b9kn1s0sv5ck9kgc2rfl";
-          })];
-      });
+    opencv3 = pkgs.opencv3.override {
+      enableContrib = true;
+      enableEigen = true;
+      enableFfmpeg = true;
+      enablePython = true;
+    };
+
+    # opencv3 =
+    #   let mypy27 = pkgs.python27.buildEnv.override {
+    #         extraLibs = [ pkgs.python27Packages.numpy ];
+    #       };
+    #       mypy3 = pkgs.python3.buildEnv.override {
+    #         extraLibs = [ pkgs.python3Packages.numpy ];
+    #       };
+    #   in (pkgs.opencv3.override {enableContrib = true;}).overrideDerivation
+    #   (oldAttrs: {
+    #     buildInputs = (with pkgs; [ unzip libjpeg libpng libtiff ])
+    #       ++ [ pkgs.eigen pkgs.bzip2 mypy27 mypy3 pkgs.ffmpeg-full ];
+    #     nativeBuildInputs = oldAttrs.nativeBuildInputs
+    #       ++ [ pkgs.doxygen ]
+    #       ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
+    #            (with pkgs.darwin.apple_sdk.frameworks;
+    #             [ AVFoundation OpenCL QuartzCore QTKit AppKit Cocoa ]);
+    #     preConfigure = ''
+    #       NIX_CFLAGS_COMPILE=$(echo "$NIX_CFLAGS_COMPILE" | ${pkgs.gnused}/bin/sed "s,[[:space:]]*-F$NIX_STORE/[[:alnum:]]*-CF-osx-[[:digit:].]*/Library/Frameworks,,g")
+    #       export PYTHON2_EXECUTABLE=${mypy27}/bin/python
+    #       export PYTHON2_LIBRARY=${mypy27}/lib/libpython2.7.dylib
+    #       export PYTHON2_INCLUDE_DIR=${mypy27}/include/python2.7
+    #       export PYTHON2_PACKAGES_PATH=${mypy27}/lib/python2.7/site-packages
+    #       export PYTHON3_EXECUTABLE=${mypy3}/bin/python3
+    #       export PYTHON3_LIBRARY=${mypy3}/lib/libpython3.5m.dylib
+    #       export PYTHON3_INCLUDE_DIR=${mypy3}/include/python3.5
+    #       export PYTHON3_PACKAGES_PATH=${mypy3}/lib/python3.5/site-packages
+    #       export EIGEN_ROOT=${pkgs.eigen}
+    #       ${pkgs.gnused}/bin/sed -i 's,\(^[[:space:]]*PATH_SUFFIXES include/eigen3 include/eigen2 Eigen/include/eigen3 Eigen/include/eigen2\),\1 eigen3,' ./cmake/OpenCVFindLibsPerf.cmake
+    #       ${pkgs.gnused}/bin/sed -i 's|    INSTALL_NAME_DIR lib|    INSTALL_NAME_DIR ''${CMAKE_INSTALL_NAME_DIR}|' ./cmake/OpenCVModule.cmake
+    #     '';
+    #     cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DWITH_FFMPEG=ON -DWITH_QT=OFF -DWITH_QUICKTIME=OFF -DWITH_AVFOUNDATION=ON" ];
+    #     NIX_CFLAGS_COMPILE="-DDEPLOYMENT_TARGET_MACOSX";
+    #     patches = [
+    #       (pkgs.fetchpatch {
+    #          url = https://github.com/opencv/opencv/commit/a2bda999211e8be9fbc5d40038fdfc9399de31fc.patch;
+    #          sha256 = "1dmq6gqsy1rzf57acr6kn4xy03vdmmk9b9kn1s0sv5ck9kgc2rfl";
+    #       })];
+    #   });
 
     # ros = (pkgs.callPackage ~/Documents/Projects/Nix/Ros).indigo.perception;
 
