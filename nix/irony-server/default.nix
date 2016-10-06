353fe32d2f6ac260d505ebe87eb7cf25265eb5fb
Anthony Cowley <acowley@gmail.com>
2016-10-06 15:59:21 -0400

    Reflect the new location of the irony-server nix

--- nix/irony-server/default.nix
+++ nix/irony-server/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
   #   rev = "11bdf09b998c54be59bac8df3368039de2c0aa19";
   #   sha256 = "1q0liagda4ch9ya5ic9flwprxv4wq23ws3kg00d4wg0y3x4czwhm";
   # };
-  src = ../../src/irony-mode;
+  src = ../../../src/irony-mode;
   buildInputs = [ cmake ];
   preConfigure = "cd server";
   cmakeFlags = [
