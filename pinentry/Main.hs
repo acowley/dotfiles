f429ab07afab48669f77828581a8dc9b1e40157f
Anthony Cowley <acowley@gmail.com>
2016-12-09 15:52:30 -0500

    Fixed bug in pinentry protocol
    An "OK" string was accidentally an "Ok" string.
    
    Added a logging feature to help debug such issues, but keeping it
    commented out.
    
    I've also commented out the emacs server test as it proved unreliable.

--- pinentry/Main.hs
+++ pinentry/Main.hs
@@ -1,10 +1,18 @@
+-- Build with:
+-- nix-shell -p 'haskellPackages.ghcWithPackages (p: [p.directory p.time])'
+-- ghc Main.hs -o hpinentry
 import Control.Arrow (second)
 import Control.Exception (bracket_)
+import Control.Monad (when)
 import Data.Char (isSpace)
 import Data.Maybe (fromMaybe)
+import Data.Time
+import System.Directory (doesFileExist, removeFile, renameFile)
 import System.Exit (exitSuccess, ExitCode(..))
 import System.IO (hFlush, stdout, hGetEcho, hSetEcho, stdin)
+import System.Posix.Files (getFileStatus, fileSize)
 import System.Posix.Process (getProcessID, executeFile)
+import System.Posix.Types (FileOffset)
 import System.Process (callCommand, readProcess, readProcessWithExitCode)
 
 data PinEntry = PinEntry { desc      :: Maybe String
@@ -29,9 +37,11 @@ mkSexp d p o e =
   d++"\" \""++p++"\" \""++o++"\" \""++e++"\")"
 
 pinEntry :: PinEntry -> IO ()
-pinEntry pe = getLine >>= uncurry aux . second (drop 1) . break isSpace
+pinEntry pe = getLine >>= uncurry aux' . second (drop 1) . break isSpace
   where k = putStrLn "OK" >> hFlush stdout >> pinEntry pe
         k' pe' = putStrLn "OK" >> hFlush stdout >> pinEntry pe'
+        aux' cmd arg = logMsg ("Got command "++cmd++" with argument " ++ arg)
+                       >> aux cmd arg
         aux "SETDESC" x = k' pe { desc = Just (clean x) }
         aux "SETPROMPT" x = k' pe { prompt = Just x }
         aux "SETOK" x = k' pe { ok = Just x }
@@ -41,9 +51,9 @@ pinEntry pe = getLine >>= uncurry aux . second (drop 1) . break isSpace
                                (fromMaybe "PROMPT" (prompt pe))
                                (fromMaybe "OK" (ok pe))
                                (fromMaybe "ERROR" (errorMsg pe))
-             sout <- readProcess 
-                       "/Users/acowley/.nix-profile/bin/emacsclient" 
-                       ["-e", sexp] 
+             sout <- readProcess
+                       "/Users/acowley/.nix-profile/bin/emacsclient"
+                       ["-e", sexp]
                        []
              let pw = takeWhile (/= '"') (dropWhile (== '"') sout)
              putStrLn ("D " ++ pw) >> k
@@ -57,27 +67,56 @@ pinEntry pe = getLine >>= uncurry aux . second (drop 1) . break isSpace
                            Nothing -> exitSuccess
                            Just f -> do callCommand ("touch "++f)
                                         exitSuccess
-        aux "GETINFO" x = 
+        aux "GETINFO" x =
           do case x of
                "pid" -> getProcessID >>= putStrLn . ("D "++) . show
                _ -> return ()
              k
         aux _ _ = k
-        
+
 emacsServerRunning :: IO Bool
-emacsServerRunning = 
-  do (code, _, _) <- readProcessWithExitCode 
-                       "emacsclient" 
-                       ["-a", "false", "-e", "0"] 
+emacsServerRunning =
+  do (code, _, _) <- readProcessWithExitCode
+                       "emacsclient"
+                       ["-a", "false", "-e", "0"]
                        ""
      return (code == ExitSuccess)
 
 main :: IO ()
-main = 
-  do go <- emacsServerRunning
-     if go
-       then do putStrLn "Ok"
-               hFlush stdout
-               pinEntry $ PinEntry Nothing Nothing Nothing Nothing Nothing Nothing
-       else executeFile "pinentry-curses" True [] Nothing
+main =
+  do putStrLn "OK"
+     hFlush stdout
+     pinEntry $ PinEntry Nothing Nothing Nothing Nothing Nothing Nothing
 
+logMsg :: String -> IO ()
+logMsg _ = return ()
+
+-- getFileSize :: String -> IO FileOffset
+-- getFileSize path = do
+--     stat <- getFileStatus path
+--     return (fileSize stat)
+
+-- logMsg :: String -> IO ()
+-- logMsg msg =
+--   do t <- getCurrentTime
+--      let stamp = formatTime defaultTimeLocale "%F %X" t
+--      appendFile logFile ('[': stamp ++ "] " ++ msg ++ "\n")
+--      sz <- getFileSize logFile
+--      when (sz > 1024*1024) $
+--        do bakExists <- doesFileExist old
+--           when bakExists (removeFile old)
+--           renameFile logFile old
+--   where logFile = "/tmp/hpinentry-log"
+--         old = "/tmp/hpinentry-log-old"
+
+-- main :: IO ()
+-- main =
+--   do logMsg "Starting hpinentry"
+--      go <- emacsServerRunning
+--      if True || go
+--        then do logMsg "emacs server is running"
+--                putStrLn "OK"
+--                hFlush stdout
+--                pinEntry $ PinEntry Nothing Nothing Nothing Nothing Nothing Nothing
+--        else do logMsg "exec'ing pinentry-curses"
+--                executeFile "pinentry-curses" True [] Nothing
