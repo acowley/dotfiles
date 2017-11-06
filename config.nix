c2580e8294e80358d46e11a2bfb22e7112d4b9c9
Anthony Cowley <acowley@gmail.com>
2017-11-06 14:16:18 -0500

    config.nix cleanup

--- config.nix
+++ config.nix
@@ -2,31 +2,23 @@
   allowUnfree = true;
   allowBroken = true;
   packageOverrides = pkgs: rec {
-    darwin = pkgs.darwin // {
-      # osx_sdk = pkgs.callPackage <nixpkgs/pkgs/os-specific/darwin/osx-sdk/10.11.nix> {};
-      osx_sdk = pkgs.callPackage ./nix/osx-sdk/10.11.nix {};
-    };
-    pass = with pkgs;
-           callPackage (<nixpkgs> + /pkgs/tools/security/pass) { x11Support = false; };
     global = pkgs.callPackage ./nix/global {};
-    libigl = pkgs.callPackage ./nix/libigl {};
     emacs = pkgs.emacs25Macport;
     emacsMacPackagesNg = pkgs.emacsPackagesNgGen emacs;
+
     pcl_demo = pkgs.callPackage ./nix/pcl_demo {};
+
+    isync = pkgs.callPackage ./nix/isync {};
 
     easyloggingpp = pkgs.callPackage ./nix/easyloggingpp {};
 
     gtsam = pkgs.callPackage ./nix/gtsam {};
 
-    # mu = pkgs.mu.override {
-    #   gtk3 = null;
-    #   webkitgtk24x = null;
-    # };
     nixBufferBuilders = import (<nixpkgs> + /pkgs/build-support/emacs/buffer.nix) {
       inherit (pkgs) lib writeText;
       inherit (emacsMacPackagesNg) inherit-local;
     };
-    irony-server = pkgs.callPackage ./nix/irony-server {};
+
     mygnused = pkgs.stdenv.mkDerivation {
       name = "mygnused";
       buildInputs = [pkgs.gnused];
@@ -46,35 +38,13 @@
       };
     };
 
-    gnat = pkgs.wrapCC (pkgs.gcc.cc.override {
-          name = "gnat";
-          langCC  = false;
-          langC   = true;
-          langAda = true;
-          profiledCompiler = false;
-
-          # We can't use the ppl stuff, because we would have
-          # libstdc++ problems.
-          # cloogppl = null;
-          # ppl = null;
-    });
-
-    # octave = pkgs.callPackage (<nixpkgs> + /pkgs/development/interpreters/octave) {
-    #   openblas = pkgs.openblasCompat;
-    #   jdk = null;
-    # };
-    # octave = pkgs.octave.override {
-    #   ghostscript = pkgs.ghostscript;
-    #   graphicsmagick = pkgs.graphicsmagick;
-    #   llvm = pkgs.llvm;
-    #   hdf5 = pkgs.pdf5;
-    #   glpk = pkgs.glpk;
-    #   suitesparse = pkgs.suitesparse;
-    # };
     octaveFull = pkgs.octaveFull.override {
       openblas = if pkgs.stdenv.isDarwin then pkgs.openblasCompat else pkgs.openblas;
       suitesparse = pkgs.suitesparse;
       jdk = null;
+    };
+    octave-image = pkgs.callPackage ./nix/octave-image {
+      octave = octaveFull;
     };
     fltk = pkgs.callPackage ./nix/fltk {};
 
@@ -83,28 +53,13 @@
       inherit (pkgs) nix cacert;
     };
 
-    gazebo7 = pkgs.callPackage ./nix/gazebo/7.nix {};
-    sdformat4 = pkgs.callPackage (<nixpkgs> + /pkgs/development/libraries/sdformat) { };
-    sdformat = sdformat4;
-    ogre = pkgs.callPackage ./nix/ogre/default.nix {};
+    # gazebo7 = pkgs.callPackage ./nix/gazebo/7.nix {};
+    # sdformat4 = pkgs.callPackage (<nixpkgs> + /pkgs/development/libraries/sdformat) { };
+    # sdformat = sdformat4;
+    # ois = pkgs.callPackage ./nix/ois {};
+    # ogre = pkgs.callPackage ./nix/ogre {};
 
     busybox = null;
-
-    glfw = with pkgs; callPackage ./nix/glfw/3.x.nix {};
-
-    # libdevil = (pkgs.libdevil.override {
-    #   mesa=null; libX11=null;
-    # }).overrideDerivation (_: {
-    #     buildInputs = with pkgs;
-    #       [ libjpeg libpng libmng lcms1 libtiff openexr ]
-    #         ++ (if stdenv.isDarwin
-    #             then [darwin.apple_sdk.frameworks.OpenGL]
-    #             else [mesa libX11]);
-    # });
-
-    # graphviz = (pkgs.graphviz.override {
-    #   xorg = null;
-    # });
 
     uriparser = pkgs.uriparser.overrideDerivation (_: {
       configureFlags = ["--disable-doc"];
@@ -115,13 +70,21 @@
                   in [ p.setuptools p.pip p.bottle p.platformio ];
     }).env;
 
-    # opencv3 = pkgs.opencv3.override {
+    opencv32 = pkgs.callPackage ./nix/opencv/3.2.nix {
+      enableContrib = true;
+      enableEigen = true;
+      enableFfmpeg = true;
+      enablePython = true;
+      inherit (pkgs.darwin.apple_sdk.frameworks)
+        AVFoundation Cocoa QTKit VideoDecodeAcceleration;
+    };
+
     opencv3 = pkgs.callPackage ./nix/opencv/3.x.nix {
       enableContrib = true;
       enableEigen = true;
       enableFfmpeg = true;
       enablePython = true;
-      inherit (darwin.apple_sdk.frameworks)
+      inherit (pkgs.darwin.apple_sdk.frameworks)
         AVFoundation Cocoa QTKit VideoDecodeAcceleration;
     };
 
@@ -141,47 +104,6 @@
     atlas = pkgs.atlas.overrideDerivation (_: {
       doCheck = false;
     });
-
-    # opencv3 =
-    #   let mypy27 = pkgs.python27.buildEnv.override {
-    #         extraLibs = [ pkgs.python27Packages.numpy ];
-    #       };
-    #       mypy3 = pkgs.python3.buildEnv.override {
-    #         extraLibs = [ pkgs.python3Packages.numpy ];
-    #       };
-    #   in (pkgs.opencv3.override {enableContrib = true;}).overrideDerivation
-    #   (oldAttrs: {
-    #     buildInputs = (with pkgs; [ unzip libjpeg libpng libtiff ])
-    #       ++ [ pkgs.eigen pkgs.bzip2 mypy27 mypy3 pkgs.ffmpeg-full ];
-    #     nativeBuildInputs = oldAttrs.nativeBuildInputs
-    #       ++ [ pkgs.doxygen ]
-    #       ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
-    #            (with pkgs.darwin.apple_sdk.frameworks;
-    #             [ AVFoundation OpenCL QuartzCore QTKit AppKit Cocoa ]);
-    #     preConfigure = ''
-    #       NIX_CFLAGS_COMPILE=$(echo "$NIX_CFLAGS_COMPILE" | ${pkgs.gnused}/bin/sed "s,[[:space:]]*-F$NIX_STORE/[[:alnum:]]*-CF-osx-[[:digit:].]*/Library/Frameworks,,g")
-    #       export PYTHON2_EXECUTABLE=${mypy27}/bin/python
-    #       export PYTHON2_LIBRARY=${mypy27}/lib/libpython2.7.dylib
-    #       export PYTHON2_INCLUDE_DIR=${mypy27}/include/python2.7
-    #       export PYTHON2_PACKAGES_PATH=${mypy27}/lib/python2.7/site-packages
-    #       export PYTHON3_EXECUTABLE=${mypy3}/bin/python3
-    #       export PYTHON3_LIBRARY=${mypy3}/lib/libpython3.5m.dylib
-    #       export PYTHON3_INCLUDE_DIR=${mypy3}/include/python3.5
-    #       export PYTHON3_PACKAGES_PATH=${mypy3}/lib/python3.5/site-packages
-    #       export EIGEN_ROOT=${pkgs.eigen}
-    #       ${pkgs.gnused}/bin/sed -i 's,\(^[[:space:]]*PATH_SUFFIXES include/eigen3 include/eigen2 Eigen/include/eigen3 Eigen/include/eigen2\),\1 eigen3,' ./cmake/OpenCVFindLibsPerf.cmake
-    #       ${pkgs.gnused}/bin/sed -i 's|    INSTALL_NAME_DIR lib|    INSTALL_NAME_DIR ''${CMAKE_INSTALL_NAME_DIR}|' ./cmake/OpenCVModule.cmake
-    #     '';
-    #     cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DWITH_FFMPEG=ON -DWITH_QT=OFF -DWITH_QUICKTIME=OFF -DWITH_AVFOUNDATION=ON" ];
-    #     NIX_CFLAGS_COMPILE="-DDEPLOYMENT_TARGET_MACOSX";
-    #     patches = [
-    #       (pkgs.fetchpatch {
-    #          url = https://github.com/opencv/opencv/commit/a2bda999211e8be9fbc5d40038fdfc9399de31fc.patch;
-    #          sha256 = "1dmq6gqsy1rzf57acr6kn4xy03vdmmk9b9kn1s0sv5ck9kgc2rfl";
-    #       })];
-    #   });
-
-    # ros = (pkgs.callPackage ~/Documents/Projects/Nix/Ros).indigo.perception;
 
     myPythonEnv = pkgs.python3.buildEnv.override {
       extraLibs = with pkgs.python3Packages; [
