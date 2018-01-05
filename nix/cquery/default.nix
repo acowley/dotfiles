135f73addb0282c98f1685c74d3ebd9d91b8d9f3
Anthony Cowley <acowley@gmail.com>
2018-01-05 14:43:20 -0500

    cquery update
    This required a patch to build with nix

--- nix/cquery/default.nix
+++ nix/cquery/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub, python, git, llvmPackages }:
 stdenv.mkDerivation rec {
   name = "cquery-${version}";
-  version = "2017-12-27";
+  version = "2018-01-05";
 
   src = fetchFromGitHub {
     owner = "jacobdufault";
     repo = "cquery";
-    rev = "183b9626c61652c2053d5cf63062c0eef392177a";
-    sha256 = "0gk558b731nrrmzs0vx6bjyhrsxgfl7rc0i4g3xg2m0mdsnnx6i4";
+    rev = "84fbc1660a02d8742d8cbc4c2670526e52f9e2e1";
+    sha256 = "0x1cb7m30i2zfyx7hk9mfvw06s72yy0x200nr53fyy602vswnsm1";
     fetchSubmodules = true;
   };
   buildInputs = [ python git llvmPackages.llvm llvmPackages.clang llvmPackages.clang-unwrapped ];
@@ -15,6 +15,8 @@ stdenv.mkDerivation rec {
   configurePhase = ''
     ./waf configure --use-system-clang --prefix=$out
   '';
+
+  patches = [ ./clang-on-path.patch ];
 
   buildPhase = ''
     ./waf build
