ac1da7bf19797c0e931f84612ade7a59e4a04fd2
Anthony Cowley <acowley@gmail.com>
2018-01-06 23:15:00 -0500

    Add nix-cquery compile_commands.json bash helper

--- nix/cquery/default.nix
+++ nix/cquery/default.nix
@@ -28,6 +28,29 @@ stdenv.mkDerivation rec {
     cp emacs/cquery.el $out/share/emacs/site-lisp
   '';
 
+  # This helper is provided to help pass include directories nix
+  # communicates in the environment to cquery through a
+  # compile_commands.json. Such a file will record a compiler
+  # invocation that might work to build a program, but this may rely
+  # on the compiler being a nix-built wrapper that adds include paths
+  # gleaned from the environment. Tooling like cquery needs to know
+  # what the compiler knows, so we dig through the environment and
+  # augment the compiler command to explicitly include the headers
+  # from the nix environment variable. We also pick out the Libsystem
+  # include directory needed on darwin.
+  setupHook = builtins.toFile "setup-hook.sh" ''
+    nix-cquery() {
+      echo "Adding Nix include directories to the compiler commands"
+      if [ -f compile_commands.json ]; then
+        local extraincs=$(echo $NIX_CFLAGS_COMPILE | awk '{ for(i = 1; i <= NF; i++) if($i == "-isystem") printf "-isystem %s ", $(i+1) }' | sed 's|-isystem \([^[:space:]]*libc++[^[:space:]]*\)|-isystem \1 -isystem \1/c++/v1 |'; cat $(dirname $(which clang++))/../nix-support/libc-cflags | awk '{ for(i = 1; i <= NF; i++) if($i == "-idirafter" && $(i+1) ~ /Libsystem/) printf "-isystem %s", $(i+1) }')
+        sed "s|/bin/clang++ |/bin/clang++ ''${extraincs} |" -i compile_commands.json
+      else
+        echo "There is no compile_commands.json file to edit!"
+        echo "Create one with cmake using:"
+        echo "(cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. && mv compile_commands.json ..)"
+      fi
+    }
+  '';
   meta = {
     description = "Low-latency language server for C++, powered by libclang";
     homepage = https://github.com/jacobdufault/cquery;
