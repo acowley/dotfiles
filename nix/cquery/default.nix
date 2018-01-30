3dab95a465a8b81759c393a61a683a03dbaf0105
Anthony Cowley <acowley@gmail.com>
2018-01-29 23:53:36 -0500

    Fix a clang standard include directory
    Discovered that cquery wasn't finding x86intrin.h. This is now fixed.

--- nix/cquery/default.nix
+++ nix/cquery/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, python, git, llvmPackages }:
+{ stdenv, fetchFromGitHub, writeTextFile, python, git, llvmPackages }:
 stdenv.mkDerivation rec {
   name = "cquery-${version}";
   version = "2018-01-29";
@@ -11,9 +11,24 @@ stdenv.mkDerivation rec {
     fetchSubmodules = true;
   };
 
-  buildInputs = [ python git llvmPackages.clang llvmPackages.clang-unwrapped ];
+  buildInputs = [ python git llvmPackages.clang llvmPackages.clang-unwrapped llvmPackages.libclang ];
+
+  # The header files we want cquery to find are under the
+  # /nix/store/xxx-clang-version/lib/clang/version/include directory, while
+  # the libclang it needs is in /nix/store/xxx-clang/lib so we can not
+  # give it a single --clang-prefix that locates everything it needs.
+  preConfigure = ''
+    sed -e 's|^\([[:space:]]*\)\(prefix = ctx.root.find_node(ctx.options.clang_prefix)\)|\1\2\
+\1prefixInc = ctx.root.find_node("${llvmPackages.clang-unwrapped}/lib/clang/${stdenv.lib.getVersion llvmPackages.clang.name}")\
+\1prefixLib = ctx.root.find_node("${llvmPackages.libclang}")|' \
+        -e 's|if not prefix:|if False:|' \
+        -e 's|\(includes = \[ n.abspath() for n in \[ prefix\)|\1Inc|' \
+        -e 's|\(libpath  = \[ n.abspath() for n in \[ prefix\)|\1Lib|' \
+        -i wscript
+      '';
 
   configurePhase = ''
+    eval "$preConfigure"
     ./waf configure --use-system-clang --prefix=$out --clang-prefix=${llvmPackages.clang-unwrapped} --llvm-config=
   '';
 
@@ -35,9 +50,11 @@ stdenv.mkDerivation rec {
   # augment the compiler command to explicitly include the headers
   # from the nix environment variable. We also pick out the Libsystem
   # include directory needed on darwin.
-  setupHook = builtins.toFile "setup-hook.sh" ''
+  setupHook = writeTextFile {
+    name = "setup-hook.sh";
+    text = ''
     nix-cflags-include() {
-      echo $NIX_CFLAGS_COMPILE | awk '{ for(i = 1; i <= NF; i++) if($i == "-isystem") printf "-isystem %s ", $(i+1); else if($i ~ /^-F/) printf "%s ", $i; }' | sed 's|-isystem \([^[:space:]]*libc++[^[:space:]]*\)|-isystem \1 -isystem \1/c++/v1|'; cat $(dirname $(which clang++))/../nix-support/libc-cflags | awk '{ for(i = 1; i <= NF; i++) if($i == "-idirafter" && $(i+1) ~ /Libsystem/) printf "-isystem %s", $(i+1) }'
+      echo $NIX_CFLAGS_COMPILE | awk '{ for(i = 1; i <= NF; i++) if($i == "-isystem") printf "-isystem %s ", $(i+1); else if($i ~ /^-F/) printf "%s ", $i; }' | sed 's|-isystem \([^[:space:]]*libc++[^[:space:]]*\)|-isystem \1 -isystem \1/c++/v1|'; printf "%s " "-isystem ${llvmPackages.clang-unwrapped}/lib/clang/${stdenv.lib.getVersion llvmPackages.clang.name}/include"; cat $(dirname $(which clang++))/../nix-support/libc-cflags | awk '{ for(i = 1; i <= NF; i++) if($i == "-idirafter" && $(i+1) ~ /Libsystem/) printf "-isystem %s", $(i+1) }'
     }
     nix-cquery() {
       if [ -f compile_commands.json ]; then
@@ -56,7 +73,7 @@ stdenv.mkDerivation rec {
         esac
       fi
     }
-  '';
+  '';};
   meta = {
     description = "Low-latency language server for C++, powered by libclang";
     homepage = https://github.com/jacobdufault/cquery;
