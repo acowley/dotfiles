369f02ecad9e72ab96fd5e76c6b86ab96045629b
Anthony Cowley <acowley@gmail.com>
2018-08-03 12:43:01 -0400

    Updated hcc sources hash

--- nix/hcc/hcc-sources.nix
+++ nix/hcc/hcc-sources.nix
@@ -4,7 +4,7 @@
 
   # 1.8.x branch
   rev = "47899bc8475b3762e2165016a0c9916547a90670";
-  sha256 = "0v3xy33dd2bbg88lx55vssqgr8aqg9dfid2krihpqm8qcbz4gw91";
+  sha256 = "1b02mx2clgpgxj4ffzf74zczdydc88f99vhysd66jkdk84s2kq0b";
 
   fetchSubmodules = true;
   leaveDotGit = true;
