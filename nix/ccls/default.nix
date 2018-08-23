db23128ecd5bffb6e3c2d08d6d0448515130a0c6
Anthony Cowley <acowley@gmail.com>
2018-08-23 10:20:24 -0400

    ccls update

--- nix/ccls/default.nix
+++ nix/ccls/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchFromGitHub, cmake, llvmPackages_6 }:
 llvmPackages_6.stdenv.mkDerivation rec {
   name = "ccls-${version}";
-  version = "20180730";
+  version = "20180823";
   src = fetchFromGitHub {
     owner = "MaskRay";
     repo = "ccls";
-    rev = "924fedbb02be07e19fd2a5372a09e89779973d41";
-    sha256 = "01lgfk3x0n3f40h83bps8h515n4psynsvy40g1a3ry1r0lns87c6";
+    rev = "ec2b893ee4a3a2d90a5458cc6bdc6a2c7379219f";
+    sha256 = "05rwm78pl9x7crlxjik35wx4rp8k08yn82n2q643d3n325bc0lpj";
     fetchSubmodules = true;
   };
   enableParallelBuilding = true;
@@ -21,7 +21,7 @@ llvmPackages_6.stdenv.mkDerivation rec {
   meta = {
     description = "C/C++/ObjC language server";
     homepage = https://github.com/MaskRay/ccls;
-    license = stdenv.lib.licenses.mit;
+    license = stdenv.lib.licenses.asl20;
     platforms = stdenv.lib.platforms.all;
   };
 }
