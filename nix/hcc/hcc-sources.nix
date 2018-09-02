a61e7f0827274c0513a135b5609c2c4190340738
Anthony Cowley <acowley@gmail.com>
2018-09-01 22:24:31 -0400

    hcc submodule update again caused a sha256 change

--- nix/hcc/hcc-sources.nix
+++ nix/hcc/hcc-sources.nix
@@ -4,7 +4,7 @@
 
   # 1.8.x branch
   rev = "47899bc8475b3762e2165016a0c9916547a90670";
-  sha256 = "1b02mx2clgpgxj4ffzf74zczdydc88f99vhysd66jkdk84s2kq0b";
+  sha256 = "12am47p98v4hf1a4ygvh0y5p4dir2v685y4mm2hss1hp8spxld4g";
 
   fetchSubmodules = true;
   leaveDotGit = true;
