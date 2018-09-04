de5f63a42278749ae6f9bc4cbb57b7c4c258231b
Anthony Cowley <acowley@gmail.com>
2018-09-04 13:56:30 -0400

    Revert "ccls update"
    This reverts commit 6f2e6db6f23a9b8eb6a1032b5e0775d0b79e0e52.

--- nix/ccls/default.nix
+++ nix/ccls/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchFromGitHub, cmake, llvmPackages_6 }:
 llvmPackages_6.stdenv.mkDerivation rec {
   name = "ccls-${version}";
-  version = "20180902";
+  version = "20180823";
   src = fetchFromGitHub {
     owner = "MaskRay";
     repo = "ccls";
-    rev = "11e92d52a898cde8cb788390034a033441e4ba12";
-    sha256 = "036qia8n9k741ixknmnrlwskfm4mvl046mh4k9jhl8pm31d9v50b";
+    rev = "ec2b893ee4a3a2d90a5458cc6bdc6a2c7379219f";
+    sha256 = "05rwm78pl9x7crlxjik35wx4rp8k08yn82n2q643d3n325bc0lpj";
     fetchSubmodules = true;
   };
   enableParallelBuilding = true;
