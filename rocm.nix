c5367293309d6cf4a57d204353ccee4132978b09
Anthony Cowley <acowley@gmail.com>
2018-12-13 15:39:24 -0500

    Removed most of my old personal rocm overlay
    The good (maintained) parts are all in the nixos-rocm repository.

--- rocm.nix
+++ rocm.nix
@@ -1,48 +1,12 @@
 self: super:
 {
-  # hcc-clang-unwrapped = super.callPackage ./nix/hcc/clang.nix {};
-  # hcc-env = let hccself = {
-  #   clang = super.wrapCCWith {
-  #     cc = self.hcc-clang-unwrapped;
-  #     bintools = super.binutils;
-  #     libc = super.binutils.libc;
-  #     extraBuildCommands = ''
-  #       mkdir -p $out/include
-  #       ln -s ${self.hcc-clang-unwrapped}/include $out/include/hcc
-  #       ln -s $out/bin/clang++ $out/bin/hcc
-  #     '';
-  #   };
-  #   stdenv = super.stdenvAdapters.overrideCC super.stdenv hccself.clang;
-  # }; in hccself;
-  # hcc-clang = self.hcc-env.clang;
-  # hcc = self.callPackage ./nix/hcc/hcc.nix {};
-  # hip = self.callPackage ./nix/hip {};
-
-  hipblas-check = self.callPackage ./nix/hipblas { doCheck = true; };
-  hipblas = self.callPackage ./nix/hipblas {};
-  rocblas-check = self.callPackage ./nix/rocblas { doCheck = true; };
-  rocblas = self.callPackage ./nix/rocblas {};
-
-  rocrand-check = self.callPackage ./nix/rocrand { doCheck = true; };
-  rocrand = self.callPackage ./nix/rocrand {};
-  rocfft-check = self.callPackage ./nix/rocfft { doCheck = true; };
-  rocfft = self.callPackage ./nix/rocfft {};
-
-  # rocm-smi = self.callPackage ./nix/rocm-smi {};
-  # rocm-bandwidth = self.callPackage ./nix/rocm-bandwidth {};
-  # clang-ocl = self.callPackage ./nix/clang-ocl {};
-
-  miopengemm = self.callPackage ./nix/miopengemm {};
-  miopen = self.callPackage ./nix/miopen {};
-  miopen-hip = self.callPackage ./nix/miopen { useHip = true; };
-
   rocprim = self.callPackage ./nix/rocprim {};
   rocm-thrust = self.callPackage ./nix/rocm-thrust {};
   google-gflags-dyn = super.google-gflags.overrideAttrs (old: {
     cmakeFlags = super.lib.filter (f: isNull (builtins.match ".*STATIC.*" f)) old.cmakeFlags;
   });
 
-  rocprofiler = self.callPackage ./nix/rocprofiler {};
+  # rocprofiler = self.callPackage ./nix/rocprofiler {};
 
   rocm-caffe2 = self.callPackage ./nix/rocm-caffe2 {
     eigen3 = super.eigen3_3;
@@ -59,20 +23,8 @@ self: super:
     python-protobuf = super.python3Packages.protobuf3_1;
   };
 
-  cxlactivitylogger = super.callPackage ./nix/cxlactivitylogger {};
   hipcaffe = super.callPackage ./nix/hipcaffe {};
-  rcp = super.callPackage ./nix/rcp {};
-  # buildBazelPackage_0_4 = pkgs.buildBazelPackage.override { bazel = pkgs.bazel_0_4; };
-  buildBazelPackage_0_4 = super.callPackage (~/src/nixpkgs/pkgs/build-support/build-bazel-package/default.nix) { bazel = super.bazel_0_4; };
-  rocm-tensorflow-build = super.callPackage ./nix/rocm-tensorflow/rocm-tensorflow-build.nix {
-    bazel = super.bazel_0_4;
-    buildBazelPackage = self.buildBazelPackage_0_4;
-      inherit (super.python3Packages) python pip six numpy buildPythonPackage pythonAtLeast;
-    };
-  rocm-tensorflow = super.callPackage ./nix/rocm-tensorflow {
-    buildBazelPackage = self.buildBazelPackage_0_4;
-    inherit (super.python3Packages) python pip six numpy buildPythonPackage pythonAtLeast;
-  };
-  lapack_3_8 = super.callPackage ./nix/liblapack/3.8.nix {};
+  # rcp = super.callPackage ./nix/rcp {};
+
   mxnet = super.callPackage ./nix/mxnet { lapack = self.lapack_3_8; };
 }
