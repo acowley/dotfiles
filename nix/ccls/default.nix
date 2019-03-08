d98f914d14029a74574bcd1b4ea57847d79dedfa
Anthony Cowley <acowley@gmail.com>
2019-03-07 21:56:51 -0500

    ccls updates

--- nix/ccls/default.nix
+++ nix/ccls/default.nix
@@ -1,14 +1,13 @@
 { stdenv, fetchFromGitHub, cmake, llvmPackages_7 }:
 llvmPackages_7.stdenv.mkDerivation rec {
   name = "ccls-${version}";
-  version = "20190116";
+  version = "20190301";
   src = fetchFromGitHub {
     owner = "MaskRay";
     repo = "ccls";
-    rev = "7a79ed92b25f78f402410c37e39b755c5f8a3f81";
-    sha256 = "0q98wcrwrmjsg0yzgw5ak1hd6h2zkbr6wh84i2b5w2slrnxs6zpa";
+    rev = "a5c06232159df4994d411448ba07f853d6a3d0f1";
+    sha256 = "04bllpv57lni3z6v8jg74sxi3phzbf9y62qwpf66fcwrnmnyxp4v";
     fetchSubmodules = true;
-    # date = 2019-01-16T00:04:24+08:00;
   };
   enableParallelBuilding = true;
   nativeBuildInputs = [ cmake ];
@@ -18,6 +17,10 @@ llvmPackages_7.stdenv.mkDerivation rec {
     "-DSYSTEM_CLANG=ON"
     "-DUSE_SHARED_LLVM=ON"
   ];
+
+  preConfigure = ''
+  sed 's/\(_Clang_find_add_library(clangToolingCore)\)/_Clang_find_library(clangToolingInclusions_LIBRARY clangToolingInclusions)\nif (clangToolingInclusions_LIBRARY)\n  list(APPEND _Clang_LIBRARIES ''${clangToolingInclusions_LIBRARY})\nendif()\n\1/' -i cmake/FindClang.cmake
+  '';
 
   meta = {
     description = "C/C++/ObjC language server";
