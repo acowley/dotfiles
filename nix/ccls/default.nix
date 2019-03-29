eae4f32d07b929fe3d13d1c25bfc1317c6088aa4
Anthony Cowley <acowley@gmail.com>
2019-03-29 10:35:23 -0400

    ccls update

--- nix/ccls/default.nix
+++ nix/ccls/default.nix
@@ -1,25 +1,23 @@
 { stdenv, fetchFromGitHub, cmake, llvmPackages_7 }:
 llvmPackages_7.stdenv.mkDerivation rec {
   name = "ccls-${version}";
-  version = "20190301";
+  version = "20190326";
   src = fetchFromGitHub {
     owner = "MaskRay";
     repo = "ccls";
-    rev = "a5c06232159df4994d411448ba07f853d6a3d0f1";
-    sha256 = "04bllpv57lni3z6v8jg74sxi3phzbf9y62qwpf66fcwrnmnyxp4v";
+    # rev = "e3c1adcf4847ebe91bbe524d5d0d1ebb0a1b14a7";
+    # sha256 = "04is4bgnd4wfrpwm2d535qalrb6wmb5bf0kllhbxzzldi323bwh7";
+    rev = "304f4d7f9e576529b61b1e86262221c5f195d124";
+    sha256 = "1dpq210lh7i2l730dd6rid5h4drjgp2k2zgy0n26z5j7sj3w2j02";
     fetchSubmodules = true;
   };
   enableParallelBuilding = true;
   nativeBuildInputs = [ cmake ];
   buildInputs = [ llvmPackages_7.llvm llvmPackages_7.libclang
                   llvmPackages_7.libclang.out ];
-  cmakeFlags = [
-    "-DSYSTEM_CLANG=ON"
-    "-DUSE_SHARED_LLVM=ON"
-  ];
 
-  preConfigure = ''
-  sed 's/\(_Clang_find_add_library(clangToolingCore)\)/_Clang_find_library(clangToolingInclusions_LIBRARY clangToolingInclusions)\nif (clangToolingInclusions_LIBRARY)\n  list(APPEND _Clang_LIBRARIES ''${clangToolingInclusions_LIBRARY})\nendif()\n\1/' -i cmake/FindClang.cmake
+  preConfigure =''
+    sed 's/if(NOT LLVM_ENABLE_RTTI)/if(1)/' -i CMakeLists.txt
   '';
 
   meta = {
