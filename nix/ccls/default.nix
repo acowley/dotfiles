b2ba5cee0ed1ae36d1433c990473e65785050472
Anthony Cowley <acowley@gmail.com>
2019-04-17 21:56:40 -0400

    ccls: update

--- nix/ccls/default.nix
+++ nix/ccls/default.nix
@@ -1,20 +1,18 @@
-{ stdenv, fetchFromGitHub, cmake, llvmPackages_7 }:
-llvmPackages_7.stdenv.mkDerivation rec {
+{ stdenv, fetchFromGitHub, cmake, llvmPackages_8 }:
+llvmPackages_8.stdenv.mkDerivation rec {
   name = "ccls-${version}";
-  version = "20190326";
+  version = "20190412";
   src = fetchFromGitHub {
     owner = "MaskRay";
     repo = "ccls";
-    # rev = "e3c1adcf4847ebe91bbe524d5d0d1ebb0a1b14a7";
-    # sha256 = "04is4bgnd4wfrpwm2d535qalrb6wmb5bf0kllhbxzzldi323bwh7";
-    rev = "304f4d7f9e576529b61b1e86262221c5f195d124";
-    sha256 = "1dpq210lh7i2l730dd6rid5h4drjgp2k2zgy0n26z5j7sj3w2j02";
+    rev = "6710dbc2e9dc4b3bff1438e5be9549d147507b5d";
+    sha256 = "0i6amhgl7jjdmkisz03b3nwly5mgglhzj56kxvfyl6pnk9dzplwv";
     fetchSubmodules = true;
   };
   enableParallelBuilding = true;
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ llvmPackages_7.llvm llvmPackages_7.libclang
-                  llvmPackages_7.libclang.out ];
+  buildInputs = [ llvmPackages_8.llvm llvmPackages_8.libclang
+                  llvmPackages_8.libclang.out ];
 
   preConfigure =''
     sed 's/if(NOT LLVM_ENABLE_RTTI)/if(1)/' -i CMakeLists.txt
