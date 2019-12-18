ffebb0a566278e4b4e248ab8d85480f1a0f5c913
Anthony Cowley <acowley@gmail.com>
2019-12-17 20:19:23 -0500

    Remove old GLFW 3.1 derivation

--- config.nix
+++ config.nix
@@ -100,8 +100,6 @@
 
     clang-rf = pkgs.callPackage ./nix/clang-rf {};
 
-    glfw31 = pkgs.callPackage ./nix/glfw/3.1.nix {};
-
     pcl_demo = pkgs.callPackage ./nix/pcl_demo {};
 
     vkmark = pkgs.callPackage ./nix/vkmark {};
