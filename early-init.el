abec876c1dae2283b8003edb157642ff2bbec345
Anthony Cowley <acowley@gmail.com>
2020-01-20 01:13:46 -0500

    Minor reformatting

--- early-init.el
+++ early-init.el
@@ -1,5 +1,6 @@
 (setq gc-cons-threshold 100000000; ie 100mb, default is 800kb
       gc-cons-percentage 0.6)
+(setq frame-inhibit-implied-resize t)
 (tool-bar-mode -1)
 (setq package-enable-at-startup nil)
-(setq frame-inhibit-implied-resize t)
+
