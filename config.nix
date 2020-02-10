52f8fd75d361d91ae6a259b3c394a7d915b81dcb
Anthony Cowley <acowley@gmail.com>
2020-02-09 23:24:15 -0500

    texlive: add the `was` bundle for the gensymb package
    This provides the `\degree` macro that produces a degree symbol in
    both text and math modes.

--- config.nix
+++ config.nix
@@ -32,7 +32,7 @@
              wrapfig capt-of footmisc subdepth preview
              minifp lettrine titling titlesec fontspec todonotes
              doublestroke comment multirow cancel doi import ifoddpage
-             subfiles biblatex logreq;
+             subfiles biblatex logreq was;
     };
 
     # Apply a patch that makes nix-shell take a `-o` flag with which
