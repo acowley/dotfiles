b73898077a9374d743d0796b77f4f5fb064ec48c
Anthony Cowley <acowley@gmail.com>
2020-02-25 01:26:09 -0500

    pcl_demo: compatibility with pcl 1.10.0

--- nix/pcl_demo/default.nix
+++ nix/pcl_demo/default.nix
@@ -1,8 +1,19 @@
 { stdenv, cmake, pkgconfig, pcl, eigen, boost, darwin }:
-stdenv.mkDerivation {
+let srcFiles = [ "CMakeLists.txt" "pcl_visualizer_demo.cpp" ];
+in stdenv.mkDerivation {
   name = "pcl_demo";
   version = "0.0";
-  src = ./.;
+  # src = builtins.path {
+  #   name = "pcl_demo-src";
+  #   path = ./.;
+  #   filter = path: type: builtins.elem path srcFiles;
+  # };
+  # src = stdenv.lib.cleanSourceWith {
+  #   name = "pcl_demo-src";
+  #   filter = path: type: builtins.elem path srcFiles;
+  #   src = ./.;
+  # };
+  src = builtins.filterSource (path: type: builtins.elem (baseNameOf (toString path)) srcFiles) ./.;
   nativeBuildInputs = [ cmake pkgconfig ];
   buildInputs = [ pcl eigen boost ] ++
     stdenv.lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Cocoa;
