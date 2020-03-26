61ccb2db88f7be569eaf763c5e8f9fb71af36a55
Anthony Cowley <acowley@gmail.com>
2020-03-25 23:46:32 -0400

    Pull in a patch to make ccls work better with imenu in emacs

--- nix/ccls/default.nix
+++ nix/ccls/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, cmake, llvmPackages }:
+{ stdenv, fetchFromGitHub, fetchpatch, cmake, llvmPackages }:
 llvmPackages.stdenv.mkDerivation rec {
   name = "ccls-${version}";
   version = "20191228";
@@ -17,6 +17,18 @@ llvmPackages.stdenv.mkDerivation rec {
   preConfigure =''
     sed 's/if(NOT LLVM_ENABLE_RTTI)/if(1)/' -i CMakeLists.txt
   '';
+
+  # static symbols appear as emacs imenu leaves which prevents
+  # navigating to functions in which they are defined. This patch does
+  # not include static duration local variables in the relevant lsp
+  # query.
+  patches = [
+    (fetchpatch {
+      name = "no-static-symbols";
+      url = "https://github.com/acowley/ccls/commit/22952acd425c57de743331939f3317258978231b.patch";
+      sha256 = "1zj1fl71r98cxq0zmpvj5w487wihi1xk055fw9sbz7fai0kxxgz7";
+    })
+  ];
 
   meta = {
     description = "C/C++/ObjC language server";
