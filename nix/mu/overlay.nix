6d6cbe2f7f80b60ae23a99d8936a1c567ccbbb16
Anthony Cowley <acowley@gmail.com>
2020-04-07 23:02:27 -0400

    mu: use a pre-release version
    I was experiencing some poor performance in mu4e, so trying out a
    newer revision.

--- nix/mu/overlay.nix
+++ nix/mu/overlay.nix
@@ -4,8 +4,10 @@ self: pkgs: {
     src = pkgs.fetchFromGitHub {
       owner  = "djcb";
       repo   = "mu";
-      rev    = version;
-      sha256 = "1ciygn6skmny0smzlqqr14f2hd1dbcpka1v3c2qwiz2arvlaw2lf";
+      # rev    = version;
+      # sha256 = "1ciygn6skmny0smzlqqr14f2hd1dbcpka1v3c2qwiz2arvlaw2lf";
+      rev = "abafd658d49f0849b83d3c3fa51625a236c4c7a7";
+      sha256 = "1c41jmjf5h9vzh1nvqn0zjdqh95pyykkwwzwn4vnja26m2hpfrnb";
     };
     postPatch = "";
 
