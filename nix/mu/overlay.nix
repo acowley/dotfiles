be53cd61414912c4ae08b737cb54e2359b3b5b37
Anthony Cowley <acowley@gmail.com>
2020-04-30 00:09:26 -0400

    mu update

--- nix/mu/overlay.nix
+++ nix/mu/overlay.nix
@@ -1,15 +1,12 @@
 self: pkgs: {
   mu = pkgs.mu.overrideAttrs (old: rec {
-    version = "1.3.9";
+    version = "1.4.1";
     src = pkgs.fetchFromGitHub {
       owner  = "djcb";
       repo   = "mu";
-      # rev    = version;
-      # sha256 = "1ciygn6skmny0smzlqqr14f2hd1dbcpka1v3c2qwiz2arvlaw2lf";
-      rev = "abafd658d49f0849b83d3c3fa51625a236c4c7a7";
-      sha256 = "1c41jmjf5h9vzh1nvqn0zjdqh95pyykkwwzwn4vnja26m2hpfrnb";
+      rev    = version;
+      sha256 = "0q2ik7fj5k9i76js4ijyxbgrwqff437lass0sd5if2r40rqh0as0";
     };
-    postPatch = "";
 
     # This patch causes the mu4e-view-mode-hook to be called on
     # unread messages.  See https://github.com/djcb/mu/issues/1192
