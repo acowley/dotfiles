c4452b702f9254ec0d57f756534bebdcb4135a0f
Anthony Cowley <acowley@gmail.com>
2020-05-08 21:52:51 -0400

    rtl8821ce update

--- nix/rtl8821ce/default.nix
+++ nix/rtl8821ce/default.nix
@@ -6,8 +6,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "tomaspinho";
     repo = "rtl8821ce";
-    rev = "df0c98c00d27508381eb280d568602bd85cd8f69";
-    sha256 = "1zmyxfkrzgrjh7xzfn1j1hy08321x3vrz1ih931g1q7g40wzi6zd";
+    rev = "69765eb288a8dfad3b055b906760b53e02ab1dea";
+    sha256 = "17jiw25k74kv5lnvgycvj2g1n06hbrpjz6p4znk4a62g136rhn4s";
   };
 
   hardeningDisable = [ "pic" ];
