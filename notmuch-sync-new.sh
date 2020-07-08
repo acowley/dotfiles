be2f496c4a6a57dada1ba42e751a3769669661f6
Anthony Cowley <acowley@gmail.com>
2020-07-08 10:49:30 -0400

    A script for fetching new emails and updating the notmuch database

---
+++ notmuch-sync-new.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# The idea is to sync read flags to the server, then move our deleted
+# and archived mails to the appropriate folders using afew, then
+# remove the deleted tags (so that they are not resurfaced on threads
+# that receive new responses), then sync to the server again, and
+# finally incorporate the results of that final sync.
+mbsync gmail-inbox gmail-trash gmail-sent seas-inbox seas-trash seas-sent
+notmuch new
+afew -m
+notmuch tag -deleted folder:gmail/trash or folder:seas/trash
+mbsync gmail-inbox gmail-trash gmail-sent gmail-archive seas-inbox seas-trash seas-sent seas-archive
+notmuch new
+afew -t --new
+
+
+
