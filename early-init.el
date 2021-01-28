a7cb596d373f16ae7babdec7895f132094e2147f
Anthony Cowley <acowley@gmail.com>
2021-01-28 02:24:06 -0500

    emacs early-init tweaks to speed up startup
    Very minor effect, if any.

--- early-init.el
+++ early-init.el
@@ -4,3 +4,17 @@
 (tool-bar-mode -1)
 (setq package-enable-at-startup nil)
 
+;; From https://github.com/geza-herman/emacs/tree/fast-emacs
+
+;; remove searching for .gz files, as all my packages are uncompressed
+;; this halves how many files emacs tries to open (if you have a lot of
+;; packages, emacs executes a lot of failing file opens)
+(setq jka-compr-load-suffixes nil)
+(jka-compr-update)
+
+;; also set file-name-handler-alist to nil during 'require'
+;; this is needed because of deferred package loading
+(advice-add 'require :around (lambda (orig-fun &rest args)
+  (let ((file-name-handler-alist))
+    (apply orig-fun args))))
+
