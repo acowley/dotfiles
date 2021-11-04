b637c6b8f39077dc400c723c62bc27b909d87b05
Anthony Cowley <acowley@gmail.com>
2021-11-04 01:11:38 -0400

    Updated montserrat derivation

--- nix/montserrat.nix
+++ nix/montserrat.nix
@@ -1,17 +1,17 @@
 { fetchFromGitHub, lib }:
-let version = "7.200";
+let version = "7.222";
 in
 fetchFromGitHub {
   name = "montserrat-${version}";
   owner = "JulietaUla";
   repo = "Montserrat";
   rev = "v${version}";
-  sha256 = "0pbkg1bqkwfhqgr9gl3jpx0pr0rc5qqg2q5nxlxsca1dzvsx6swa";
+  sha256 = "sha256-UGy4oTREtYkOPt6cUjYImDIhgRPnPpa0DlOGlluNUyw=";
   postFetch = ''
     tar xf $downloadedFile --strip=1
     install -m444 -Dt $out/share/fonts/opentype/montserrat fonts/otf/*.otf
     install -m444 -Dt $out/share/fonts/ttf/montserrat      fonts/ttf/*.ttf
-    install -m444 -Dt $out/share/fonts/webfonts/montserrat fonts/*.eot fonts/*.woff fonts/*.woff2
+    install -m444 -Dt $out/share/fonts/webfonts/montserrat fonts/webfonts/*.woff fonts/webfonts/*.woff2
   '';
   meta = with lib; {
     homepage = "https://github.com/JulietaUla/Montserrat/";
