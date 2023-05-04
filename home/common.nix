559d6d98887739a49a566a1c99266e11e65400a8
Anthony Cowley <acowley@gmail.com>
2023-05-04 09:28:15 -0400

    home: trying out zoxide

--- home/common.nix
+++ home/common.nix
@@ -151,6 +151,16 @@ in {
     };
   };
 
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
