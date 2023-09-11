79dfb150ea0700c0a35d87b15a447b5b5f91930f
Anthony Cowley <acowley@gmail.com>
2023-09-11 15:50:55 -0400

    home: move environment variable definitions

--- home/home.nix
+++ home/home.nix
@@ -1,9 +1,14 @@
 { config, pkgs, ... }:
 {
-  programs.bash = {
-    sessionVariables = {
-      BORG_PASSCOMMAND="pass borg-nixos-hp";
-      BORG_REPO="raspberrypi.local:/mnt/usbdrive/backups/nixos-hp";
-    };
+  # programs.bash = {
+  #   sessionVariables = {
+  #     BORG_PASSCOMMAND="pass borg-nixos-hp";
+  #     BORG_REPO="raspberrypi.local:/mnt/usbdrive/backups/nixos-hp";
+  #   };
+  # };
+
+  home.sessionVariables = {
+    BORG_PASSCOMMAND="pass borg-nixos-hp";
+    BORG_REPO="raspberrypi.local:/mnt/usbdrive/backups/nixos-hp";
   };
 }
