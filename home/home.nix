59c212d6473fb3a1610f37fe848507a2d2d8db6c
Anthony Cowley <acowley@gmail.com>
2023-10-25 10:19:06 -0400

    home: add yaml-language-server on the home machine

--- home/home.nix
+++ home/home.nix
@@ -7,6 +7,12 @@
   #   };
   # };
 
+  home.packages = with pkgs; [
+
+    # Having this available everywhere is handy for working with YAML files
+    yaml-language-server
+  ];
+
   home.sessionVariables = {
     BORG_PASSCOMMAND="pass borg-nixos-hp";
     BORG_REPO="raspberrypi.local:/mnt/usbdrive/backups/nixos-hp";
