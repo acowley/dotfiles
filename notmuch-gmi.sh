4674ca7bb8facea9edd825b4299572a8e371579c
Anthony Cowley <acowley@gmail.com>
2026-08-29 12:40:53 -0400

    notmuch: only sync one gmail account

--- notmuch-gmi.sh
+++ notmuch-gmi.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-(cd ~/.mail/account.gmail && gmi sync && cd ~/.mail/account.serve && gmi sync) && afew -t -n
+# (cd ~/.mail/account.gmail && gmi sync && cd ~/.mail/account.serve && gmi sync) && afew -t -n
+(cd ~/.mail/account.gmail && gmi sync) && afew -t -n
